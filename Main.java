b41db14f6eba0d7aeb5fb1fcc2b0ca419a7edf25
Deadcoder11u2 <88551109+Deadcoder11u2@users.noreply.github.com>
2022-03-14 11:52:50 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,7 +7,32 @@ public class Main {
 	static PrintWriter pw;
 	
 	static void solve() throws IOException{
-		
+        int n = fs.nextInt();
+        int f_cost[] = new int[n+1];
+        int s_cost[] = new int[n+1];
+        for(int i = 0 ; i < n ; i++) {
+            f_cost[i] = fs.nextInt();
+        }
+        for(int i = 0 ; i < n ; i++) {
+            s_cost[i] = fs.nextInt();
+        }
+        int f_to_s[] = new int[n];
+        int s_to_f[] = new int[n];
+        for(int i = 1 ; i < n; i++) f_to_s[i] = fs.nextInt();
+        for(int i = 1 ; i < n; i++) s_to_f[i] = fs.nextInt();
+        Integer dp_1[] = new Integer[n+1];
+        Integer dp_2[] = new Integer[n+1];
+        dp_1[1] = f_cost[0];
+        dp_2[1] = s_cost[0];
+        for(int i = 1 ; i <= n ; i++) {
+            dp_1[i] = min(dp_1[i-1], dp_2[i-1]+s_to_f[i-1]) + f_cost[i-1];
+            dp_2[i] = min(dp_2[i-1], dp_1[i-1]+f_to_s[i-1]) + s_cost[i-1];
+        }
+        System.err.println(Arrays.toString(dp_1));
+        System.err.println(Arrays.toString(dp_2));
+        System.err.println(Arrays.toString(f_to_s));
+        System.err.println(Arrays.toString(s_to_f));
+        int first_min = Collections.min(Arrays.asList());
 	}
 
 	public static void main(String args[]) throws Exception{
